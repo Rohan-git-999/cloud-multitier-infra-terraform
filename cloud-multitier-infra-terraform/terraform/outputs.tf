5bbba846c396e0bc931af24ab508b6ddbc78e593
Rohan-git-999 <sethirohan123@gmail.com>
2025-06-30 01:15:09 +0530

    Delete cloud-multitier-infra-terraform directory

--- cloud-multitier-infra-terraform/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-  description = "Public IP of the Azure VM"
-}
-
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.vm.id
-  description = "Resource ID of the VM"
-}
